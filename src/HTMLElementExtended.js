2a70bedb4ea576b99d4a2d0fff0fb986be4620c0
Ben Rubin <ben@benjaminrubin.com>
2019-01-25 22:16:48 -0600

    inject id to HTMLElement for automatic custom id'ng

--- src/HTMLElementExtended.js
+++ src/HTMLElementExtended.js
@@ -47,6 +47,29 @@ module.exports = class HTMLElementExtended {
     let cstr = this.constructor.toString();
     // remove class declaration
     cstr = cstr.slice(cstr.indexOf('{') + 1, cstr.lastIndexOf('}'));
+    // convert to camelcase and add a '$'
+    const id = `$${name.replace(/\-([a-z])/g, m => m.toUpperCase()).replace(/-/,'')}`;
+
+    // modify / add constructor
+    const cpos = cstr.indexOf('constructor');
+    if (cpos > -1) {
+      const preConstructor = cstr.slice(0, cpos);
+      let constructor = cstr.slice(cpos);
+      const closingBracketIndex = constructor.search(/}[\n\r\s]+/g);
+      const postConstructor = constructor.slice(closingBracketIndex + 1);
+
+      constructor = constructor.slice(0, closingBracketIndex);
+      constructor += `  this.setAttribute('id', '${id}');
+  }`;
+      cstr = preConstructor + constructor + postConstructor;
+    } else {
+      cstr = `constructor() {
+        super();
+        this.setAttribute('id', '${id}');
+      }
+
+      ${cstr}`;
+    }
 
     return `
 customElements.define("${name}", class extends HTMLElement {
