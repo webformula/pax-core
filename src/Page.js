b68cbb1fa8c2851c41020a8f6f83b346b1681ffb
Ben Rubin <ben@benjaminrubin.com>
2023-01-14 14:58:18 -0600

    up

--- src/Page.js
+++ src/Page.js
@@ -66,6 +66,7 @@ export default class Page {
     await this.beforeRender();
     // TODO replace with setHTML when supported. https://developer.mozilla.org/en-US/docs/Web/API/Element/setHTML
     // currently security concerns should be mitigated by the template literal
+    
     pageContent.innerHTML = this.template.call(this, this);
     const title = document.querySelector('title');
     title.innerText = this.pageTitle;
@@ -80,75 +81,10 @@ export default class Page {
     this._urlParameters = params?.urlParameters || {};
     this._searchParameters = params?.searchParameters || {};
   }
+
+  escape(str) {
+    return str.replace(/[^\w. ]/gi, function (c) {
+      return '&#' + c.charCodeAt(0) + ';';
+    });
+  };
 }
-
-
-
-
-// --- old ---
-
-// export default class Page {
-//   // can be specified when registering page
-//   // routes = '/path';
-//   // routes = ['/a', '/b'];
-
-//   // html string or file path
-//   templateString = '';
-
-
-//   constructor() {
-//     // these are set by the route
-//     this._urlParameters = undefined;
-//     this._searchParameters = undefined;
-//   }
-
-//   set pageTitle(value) {
-//     this._pageTitle = value;
-//   }
-
-//   get searchParameters() {
-//     return this._searchParameters;
-//   }
-
-//   get urlParameters() {
-//     return this._urlParameters;
-//   }
-
-//   // override
-//   connectedCallback() { }
-//   disconnectedCallback() { }
-//   async beforeRender() { }
-//   async afterRender() { }
-
-//   // dynamic method
-//   template() {
-//     return /*html*/``;
-//   }
-
-//   async _renderTemplate() {
-//     const pageContent = document.querySelector('page-content');
-//     if (!pageContent) throw Error('Could not find <page-content>');
-
-//     await this.beforeRender();
-
-//     let renderedTemplate;
-//     if (this.templateString) renderedTemplate = new Function('page', `return \`${this.templateString}\`;`).call(this, this);
-//     else renderedTemplate = this.template.call(this, this);
-
-//     // TODO replace with setHTML when supported. https://developer.mozilla.org/en-US/docs/Web/API/Element/setHTML
-//     // currently security concerns should be mitigated by the template literal
-//     pageContent.innerHTML = renderedTemplate;
-//     document.querySelector('title').innerText = this.pageTitle;
-
-//     await this.afterRender();
-//   }
-
-//   // called by router
-//   _setUrlData(params = {
-//     urlParameters: {},
-//     searchParameters: {}
-//   }) {
-//     this._urlParameters = params?.urlParameters || {};
-//     this._searchParameters = params?.searchParameters || {};
-//   }
-// }
